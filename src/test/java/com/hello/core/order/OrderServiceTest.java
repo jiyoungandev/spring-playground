077b8811ecda440ad9543cf693423d170cd8122d
jiyoungan <ajy09844@gmail.com>
2024-12-29 23:09:16 +0900

    feat(order): add order domain

---
+++ src/test/java/com/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package com.hello.core.order;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        long memberId = 1L;
+        Member member = new Member(memberId, "member1", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "item1", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
