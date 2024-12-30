e8919c584e4a42bf5453045e62ca3a88bd6d3840
jiyoungan <ajy09844@gmail.com>
2024-12-30 22:44:25 +0900

    refactor(config): add AppConfig class

---
+++ src/main/java/com/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package com.hello.core;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        Member member = new Member(1L, "member1", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println(member.getName() == findMember.getName());
+    }
+}
